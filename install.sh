8587b4e4afe2a345949e8ed0edc85a2e6076970d
satokibi <td182007@cis.fukuoka-u.ac.jp>
2018-08-02 11:29:50 +0900

    add memo.txt / ale settings to vimrc

--- install.sh
+++ install.sh
@@ -1,13 +1,19 @@
 #!/bin/sh
 
-dir_org='~/vim_org'
+dir_org=~/vim_org
 
 if [ ! -e $dir_org ]; then
 	mkdir ~/vim_org
+elif [ -e "$dir_org/.vim" ]; then
+  rm -rf "$dir_org/.vim_"
+  mv "$dir_org/.vim" "$dir_org/.vim_"
+  rm -rf "$dir_org/.vim"
+else
+  rm -rf "$dir_org/.vim"
 fi
 
-mv ~/.vimrc ~/vim_org
-mv ~/.vim ~/vim_org
+mv ~/.vimrc $dir_org
+mv ~/.vim $dir_org
 
 ln -s ~/dotfiles/vim ~/.vim
 ln -s ~/dotfiles/vimrc ~/.vimrc
