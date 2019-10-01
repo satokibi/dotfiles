4d75c6cda2aac0d336dc5b0fe440d85583a483c7
satokibi <td182007@cis.fukuoka-u.ac.jp>
2019-10-02 01:37:22 +0900

    きれいにした!!!!!!!!!!!!!!!

--- install.sh
+++ install.sh
@@ -1,17 +1,23 @@
 #!/bin/bash
 
-has() {
-  type "$1" > /dev/null 2>&1
-}
+DOT_DIR="${HOME}/dotfiles"
+REMOTE_URL="https://github.com/satokibi/dotfiles.git"
 
-
-# brew
-if has "brew"; then
-  echo "$(tput setaf 2)Already installed Homebrew ✔︎$(tput sgr0)"
-else
-  echo "Installing Homebrew..."
-  /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+if [ ! -d ${DOT_DIR} ]; then
+  echo "Downloading dotfiles..."
+  mkdir ${DOT_DIR}
+  git clone "${REMOTE_URL}" "${DOT_DIR}"
+  echo $(tput setaf 2)Download dotfiles complete!. ✔︎$(tput sgr0)
 fi
+
+BIN_DIR="${HOME}/bin"
+if [ ! -d ${BIN_DIR} ]; then
+  mkdir ${BIN_DIR}
+fi
+
+# dotfiles
+${DOT_DIR}/etc/link.sh
+
 
 # vim plug
 if test -e ${HOME}"/.vim/autoload/plug.vim"; then
@@ -22,16 +28,14 @@ else
 fi
 
 
-DOT_DIRECTORY="${HOME}/dotfiles"
-REMOTE_URL="https://github.com/satokibi/dotfiles.git"
-
-if [ ! -d ${DOT_DIRECTORY} ]; then
-  echo "Downloading dotfiles..."
-  mkdir ${DOT_DIRECTORY}
-  git clone "${REMOTE_URL}" "${DOT_DIRECTORY}"
-  echo $(tput setaf 2)Download dotfiles complete!. ✔︎$(tput sgr0)
+# OS別
+if [ "$(uname)" == 'Darwin' ]; then
+  ${DOT_DIR}/etc/init/osx.sh
+elif [ "$(expr substr $(uname -s) 1 5)" == 'Linux' ]; then
+  echo "Linux"
+elif [ "$(expr substr $(uname -s) 1 10)" == 'MINGW32_NT' ]; then                                                                                           
+  echo "Cygwin"
+else
+  echo "Your platform ($(uname -a)) is not supported."
+  exit 1
 fi
-
-
-# dotfiles
-${DOT_DIRECTORY}/link.sh
